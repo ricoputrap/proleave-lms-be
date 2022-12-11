f3ab8d959dd75637728ec76a3dcba1fdd764ebde
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-12-11 14:53:35 +0700

    +T2 feat: Subscription Plans API - validate if all feature IDs passed in `featureIds` request body parameter are numbers

--- src/controllers/v1/SubscriptionPlan.ts
+++ src/controllers/v1/SubscriptionPlan.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from "express";
 import SubscriptionPlanService from "../../services/SubscriptionPlanService";
 import { ReturnType } from "../../types/api.types";
 import { getBadRequestResponse } from "../../utils/responseConstructor";
+import { isArrayNumeric } from "../../utils/typeChecker";
 
 const SubscriptionPlan = (): Router => {
   const router = Router();
@@ -23,7 +24,12 @@ const SubscriptionPlan = (): Router => {
       return res.status(result.code).json(result);
     }
 
-    /** @TODO validate if featureIds is not an array of number (feature IDs) */
+    /** validate if all feature IDs are numbers (numeric) */
+    if (!isArrayNumeric(featureIds)) {
+      const message = "There is at least one feature ID that is not a number.";
+      const result: ReturnType = getBadRequestResponse(message);
+      return res.status(result.code).json(result);
+    }
 
     const result: ReturnType = await service.addNewSubscriptionPlan(name, featureIds);
     return res.status(result.code).json(result);
--- src/utils/typeChecker.ts
+++ src/utils/typeChecker.ts
@@ -1,3 +1,27 @@
+/**
+ * Validate Numeric Variable
+ * =========================
+ * Check whether a variable is numeric (number) or not.
+ * @param value the variable that will be validated
+ * @returns `true` if the variable is numeric, `false` otherwise
+ */
 export const isNumeric = (value: any): boolean => {
   return /^-?\d+$/.test(value);
+}
+
+/**
+ * Validate All Numbers
+ * =====================
+ * Check whether all elements in the array are numbers (numeric)
+ * @param values array of elements that will be validated
+ * @returns `true` if all elements in the array are numbers, `false` otherwise
+ */
+export const isArrayNumeric = (values: any[]): boolean => {
+
+  for (let i = 0; i < values.length; i++) {
+    if (!isNumeric(values[i]))
+      return false;
+  }
+
+  return true;
 }
