26572b6d3d12fd40a706435363fb364ace8cdccd
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-12-18 00:30:07 +0700

    +T3 feat: feature api tests "POST - Add New Feature - WITHOUT NAME"

--- src/test/FeatureAPI/post/index.ts
+++ src/test/FeatureAPI/post/index.ts
@@ -1,8 +1,10 @@
 import { SuperTest, Test } from "supertest";
 import postBasic from "./postBasic";
+import postWithoutName from "./postWithoutName";
 
 const runPostTests = (request: SuperTest<Test>) => {
   postBasic(request);
+  postWithoutName(request);
 }
 
 export default runPostTests;
---
+++ src/test/FeatureAPI/post/postWithoutName.ts
@@ -0,0 +1,68 @@
+import { SuperTest, Test } from "supertest";
+import { STATUS_CODES } from "../../../constants/api.enum";
+
+const postWithoutName = (request: SuperTest<Test>) => {
+  describe("POST - Add New Feature - WITHOUT NAME", () => {
+    it("Add a new feature without a request body should be failed.", () => {
+      return request
+        .post("/v1/features")
+        .then(res => {
+          // the response is not empty
+          expect(res.body).toBeDefined();
+  
+          // validate the response success flag & status code
+          expect(res.body.success).toBe(false);
+          expect(res.body.code).toBe(STATUS_CODES.BAD_REQUEST);
+          expect(res.statusCode).toBe(STATUS_CODES.BAD_REQUEST);
+
+          // validate the error message
+          expect(res.body.message).toBeTruthy();
+          expect(res.body.message).toBe("The feature name is required!");
+        })
+    })
+
+    it("Add a new feature without a name in the request body should be failed.", () => {
+      const body = {};
+
+      return request
+        .post("/v1/features")
+        .send(body)
+        .then(res => {
+          // the response is not empty
+          expect(res.body).toBeDefined();
+  
+          // validate the response success flag & status code
+          expect(res.body.success).toBe(false);
+          expect(res.body.code).toBe(STATUS_CODES.BAD_REQUEST);
+          expect(res.statusCode).toBe(STATUS_CODES.BAD_REQUEST);
+
+          // validate the error message
+          expect(res.body.message).toBeTruthy();
+          expect(res.body.message).toBe("The feature name is required!");
+        })
+    })
+
+    it("Add a new feature with an empty name should be failed.", () => {
+      const body = { name: "" };
+
+      return request
+        .post("/v1/features")
+        .send(body)
+        .then(res => {
+          // the response is not empty
+          expect(res.body).toBeDefined();
+  
+          // validate the response success flag & status code
+          expect(res.body.success).toBe(false);
+          expect(res.body.code).toBe(STATUS_CODES.BAD_REQUEST);
+          expect(res.statusCode).toBe(STATUS_CODES.BAD_REQUEST);
+
+          // validate the error message
+          expect(res.body.message).toBeTruthy();
+          expect(res.body.message).toBe("The feature name is required!");
+        })
+    })
+  })
+}
+
+export default postWithoutName;
