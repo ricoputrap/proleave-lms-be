4551d15ec330beaba9d81ef654f0ab27bb9f62bf
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-01-02 11:39:32 +0700

    +T3 feat: add a unit testing for typeChecker functions

---
+++ src/utils/typeChecker.spec.ts
@@ -0,0 +1,15 @@
+import { isNumeric } from "./typeChecker";
+
+describe("UTILS - typeChecker.ts", () => {
+  it("Function: isNumeric", () => {
+    expect(isNumeric(1)).toBe(true);
+    expect(isNumeric(1)).toBe(true);
+    expect(isNumeric("1393029")).toBe(true);
+    expect(isNumeric("00001232131249")).toBe(true);
+    
+    expect(isNumeric("a name")).toBe(false);
+    expect(isNumeric("FINAL VALUE")).toBe(false);
+    expect(isNumeric("+081")).toBe(false);
+    expect(isNumeric("adasdasd")).toBe(false);
+  });
+})
